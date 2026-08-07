54a6582d62a4eac8e620f1a688b1c7fb1b5137e8
mkjai <mkjaai8964@gmail.com>
2026-08-07 10:25:45 -0700

    Loading screen

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,28 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import Navbar from './components/Navbar.vue'
 import MenuOverlay from './components/MenuOverlay.vue'
+import LoadingScreen from './components/LoadingScreen.vue'
 
 const menuOpen = ref(false)
+const loading = ref(true)
 
 function toggleMenu() {
   menuOpen.value = !menuOpen.value
 }
+
+onMounted(() => {
+  setTimeout(() => {
+    loading.value = false
+  }, 1600)
+})
 </script>
 
 <template>
+  <Transition name="loader-fade">
+    <LoadingScreen v-if="loading" />
+  </Transition>
+
   <Navbar :menu-open="menuOpen" @toggle-menu="toggleMenu" />
   <MenuOverlay :open="menuOpen" @close="menuOpen = false" />
 
---
+++ src/components/LoadingScreen.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <div class="loading-screen">
+    <div class="loading-logo">LOGO</div>
+    <div class="loading-bar-track">
+      <div class="loading-bar-fill"></div>
+    </div>
+  </div>
+</template>
